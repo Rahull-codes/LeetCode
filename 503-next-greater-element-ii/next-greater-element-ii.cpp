285e4f2ca795f76b1187a0e5380cff9e99737d0e
Rahul sahu <sahu92083@gmail.com>
2026-05-20 19:41:12 +0530

    Time: 121 ms (5.04%) | Memory: 27.8 MB (98.96%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,16 +4,6 @@ public:
         int n = nums.size();
         vector<int> res(n, -1);
 
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 1; j < n; j++) {
-        //         int idx = (i + j) % n;
-        //         if (nums[idx] > nums[i]) {
-        //             res[i] = nums[idx];
-        //             break;
-        //         }
-        //     }
-        // }
-
         for(int i = 0 ; i < n ; i++){
             for(int j = 1 ; j <n;j++){
                 int idx = (i + j) % n ;
