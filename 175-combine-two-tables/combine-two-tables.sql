939789e4aa0c6bfd2e0c221c02bf49a6d96d3d98
Rahul sahu <sahu92083@gmail.com>
2026-09-13 22:55:15 +0530

    Time: 596 ms (11.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    p.firstName,
+    p.lastName,
+    (
+        SELECT a.city
+        FROM Address a
+        WHERE a.personId = p.personId
+    ) AS city,
+    (
+        SELECT a.state
+        FROM Address a
+        WHERE a.personId = p.personId
+    ) AS state
+FROM Person p;
